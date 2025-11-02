1ea17bd3bb471969a3a0640b7883dc23f3e08838
Deepak <deepakdevopswork@gmail.com>
2025-11-02 17:53:33 +0000

    Add files via upload

---
+++ 1.provider.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_version = "<= 1.8.5" #Forcing which version of Terraform needs to be used
+  required_providers {
+    aws = {
+      version = "<= 6.0.0" #Forcing which version of plugin needs to be used.
+      source  = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket = "k8sdevopsstudy"
+    key    = "Ansible.tfstate"
+    region = "eu-west-2"
+    # dynamodb_table = "-terraform-locks"
+    encrypt = true
+  }
+}
